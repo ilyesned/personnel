1d0094714b0818ddfc385feb41f3f909ac411f56
Vaugarni ryan <ryan.vaugarni@gmail.com>
2022-04-25 17:01:14 +0200

    ajout insert et supp ligue et employe

--- Personnel/src/personnel/Passerelle.java
+++ Personnel/src/personnel/Passerelle.java
@@ -4,6 +4,15 @@ public interface Passerelle
 {
 	public GestionPersonnel getGestionPersonnel();
 	public void sauvegarderGestionPersonnel(GestionPersonnel gestionPersonnel)  throws SauvegardeImpossible;
+	/* insertion ligue */
 	public int insert(Ligue ligue) throws SauvegardeImpossible;
+	/* suppression ligue*/
+	public void deleteLigue(Ligue ligue) throws SauvegardeImpossible ;
+	/* insertion employee */
 	public int insert(Employe employe) throws SauvegardeImpossible;
+	/* suppression employe */
+	public void deleteEmploye(Employe employe) throws SauvegardeImpossible;
+	/* changer employe par l'Admin */
+	public void setAdmin(Employe employe)  throws SauvegardeImpossible;
+	
 }
